3cbd00d5a7bcd50be21e63b0beb4d214b32927c3
srouke <xpyctam4@yandex.ru>
2024-06-08 22:38:27 +0600

    cookie sol

--- internal/transport/http/cookie.go
+++ internal/transport/http/cookie.go
@@ -1,16 +1,38 @@
 package http
 
 import (
+	"errors"
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func (t *Transport) CookiesMiddlware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		cookie := &http.Cookie{
+			Name:  "session",
+			Value: "KEK",
+		}
+
+		http.SetCookie(w, cookie)
+		cookie, err := r.Cookie("exampleCookie")
+		if err != nil {
+			switch {
+			case errors.Is(err, http.ErrNoCookie):
+				http.Error(w, "cookie not found", http.StatusBadRequest)
+			default:
+				log.Println(err)
+				http.Error(w, "server error", http.StatusInternalServerError)
+			}
+			return
+		}
+		fmt.Println(cookie.Value, err)
+
 		// allCookies := r.Cookies()
 
 		// fmt.Println(allCookies)
 
-		// sessionCookie := &http.Cookie{}
+		// var sessionCookie *http.Cookie
 		// for i := 0; i < len(allCookies); i++ {
 		// 	if allCookies[i].Name == "session" {
 		// 		sessionCookie = allCookies[i]
