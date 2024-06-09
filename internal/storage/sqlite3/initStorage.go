4aed910391acb4cc3f7e72b16358c7b421516251
ynurmakh <nur_erbol_2002@mail.ru>
2024-06-09 21:39:11 +0600

    sqlite create tables

--- internal/storage/sqlite3/initStorage.go
+++ internal/storage/sqlite3/initStorage.go
@@ -2,6 +2,9 @@ package sqlite3
 
 import (
 	"database/sql"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 
 	"forum/internal/storage"
 )
@@ -12,8 +15,14 @@ type Sqlite struct {
 }
 
 func InitStorage() (storage.StorageInterface, error) {
+	db, err := openDB()
+	if err != nil {
+		return nil, err
+	}
+
 	mystorage := &Sqlite{
 		configs: "configs here",
+		db:      db,
 	}
 
 	var i storage.StorageInterface
@@ -21,4 +30,29 @@ func InitStorage() (storage.StorageInterface, error) {
 	i = mystorage
 
 	return mystorage, nil
+}
+
+func openDB() (*sql.DB, error) {
+	dbPath := "internal/storage/sqlite3/database.db"
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	chemePath := "internal/storage/sqlite3/sqlitecScheme/createTable.sql"
+	file, err := os.ReadFile(chemePath)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(string(file))
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- internal/storage/sqlite3/sqlitecScheme/createTable.sql
+++ internal/storage/sqlite3/sqlitecScheme/createTable.sql
@@ -1,18 +1,27 @@
 -- database: /home/student/forum/internal/storage/sqlite3/database.db
 
--- Use the ▷ button in the top right corner to run the entire file.
+-- OK users
+-- OK cookies
+-- OK posts
+-- OK post_category
+-- OK categoryes_name
+-- OK commemtaries
+-- OK post_reacrions
+-- OK commentary_reacrions
 
-CREATE TABLE IF NOT EXISTS cookies (
-    cookie VARCHAR(16) NOT NULL,
-    user_id INTEGER NOT NULL,
-    deadTime TIMESTAMP NOT NULL
-    );
+
+
 CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER PRIMARY KEY NOT NULL ,
     user_lvl INTEGER NOT NULL DEFAULT 1,
-    user_email VARCHAR(50) NOT NULL PRIMARY KEY,
-    user_nickname VARCHAR(50) NOT NULL PRIMARY KEY,
+    user_email VARCHAR(50) NOT NULL UNIQUE,
+    user_nickname VARCHAR(50) NOT NULL UNIQUE,
     hashed_password VARCHAR(64)
+    );
+CREATE TABLE IF NOT EXISTS cookies (
+    cookie VARCHAR(16) NOT NULL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    deadTime TIMESTAMP NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS posts (
@@ -23,10 +32,11 @@ CREATE TABLE IF NOT EXISTS posts (
     cteated_time CURRENT_TIMESTAMP NOT NULL
     );
 
-CREATE TABLE IF NOT EXISTS categries (
-    category_name INTEGER PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS categries_name (
+    category_id INTEGER PRIMARY KEY,
     category_name VARCHAR(50)
-)
+);
+
 CREATE TABLE IF NOT EXISTS posts_category (
     post_id INTEGER NOT NULL,
     category_id INTEGER NOT NULL
@@ -38,6 +48,13 @@ CREATE TABLE IF NOT EXISTS commetaries (
     commentray_content VARCHAR(2048),
     created_time CURRENT_TIMESTAMP
     );
-CREATE TABLE IF NOT EXISTS post_reactions (n int);
-
-SELECT * FROM "table-name";
+CREATE TABLE IF NOT EXISTS post_reactions(
+    user_id INTEGER,
+    post_id INTEGER,
+    reaction INTEGER
+);
+CREATE TABLE IF NOT EXISTS commentari_reactions(
+    user_id INTEGER,
+    commentaie_id INTEGER,
+    reaction INTEGER
+);
