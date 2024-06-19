257f5b206f478a58074253d54ac250afb5e043a9
srouke <xpyctam4@yandex.ru>
2024-06-19 19:40:59 +0600

    add norm cookie

--- internal/transport/http/cookie.go
+++ internal/transport/http/cookie.go
@@ -1,32 +1,32 @@
 package http
 
 import (
-	"fmt"
+	"context"
 	"net/http"
 )
 
 func (t *Transport) CookiesMiddlware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		c, err := r.Cookie("auth")
+		cookie, err := r.Cookie("auth")
 		if err != nil {
-			fmt.Fprintf(w, "You need to login first!")
-			return
-			// cookie := &http.Cookie{
-			// 	Name:  "auth",
-			// 	Value: "kek",
-			// 	Path:  "/",
-			// }
-			// http.SetCookie(w, cookie)
-		}
-		if !checkAuth(c.Value) {
-			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-			return
-		}
 
-		next(w, r)
+			uuid, err := t.service.CreateNewCookie()
+			if err != nil {
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				return
+			}
+			http.SetCookie(w, &http.Cookie{Name: "auth", Value: uuid})
+
+			ctx := context.WithValue(r.Context(), "user", nil)
+			next(w, r.WithContext(ctx))
+			return
+		}
+		user, err := t.service.GetUserByCookie(cookie.Value)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		ctx := context.WithValue(r.Context(), "user", user)
+		next(w, r.WithContext(ctx))
 	}
-}
-
-func checkAuth(s string) bool {
-	return false
 }
